f8cc988419c180f641ab4cbabc05b75ea3dbbbad
Malow <matthewlower22@gmail.com>
2026-04-07 15:26:48 -0400

    Successfully created PostgresSQL database with equipment table as outlined by java package. Runs in docker container as expected.

---
+++ src/main/java/com/malow/inventory_system/model/Equipment.java
@@ -0,0 +1,18 @@
+package com.malow.inventory_system.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Equipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Integer quantity;
+    private Double pricePerDay;
+}
